b8142b71de4e47dc02e11ef07b1839559676c0e6
MarcelAzeroth <azerothmarcel@gmail.com>
2022-04-24 17:42:52 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+//Variables
+const input = document.getElementById("input-text")
+const output = document.getElementById("output-text")
+const form = document.getElementById("sarcasto-form")
+const copy = document.getElementById("copy-button")
+
+//Functions
+
+const sarcastoTransform = function(e){
+    e.preventDefault();
+    let def = input.value.toLowerCase().split("");
+    
+    for(let i = 0; i < def.length; i++){
+        if(i % 2 === 1 ){
+            def[i] = def[i].toUpperCase();
+        }
+    }
+
+    output.innerHTML = def.join("");
+    input.value = "";
+}
+
+const copyToClipboard = function(){
+    navigator.clipboard.writeText(output.innerHTML);
+}
+
+//
+
+form.addEventListener("submit", sarcastoTransform);
+copy.addEventListener("click", copyToClipboard);
